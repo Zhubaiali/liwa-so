ed8a0e2ddc424b407b618ab06e47f23124f64677
liwa <hogwartsghls@gmail.com>
2023-05-27 18:51:41 +0800

    MySQL给canal授权

--- src/main/java/com/liwa/liwaso/MainApplication.java
+++ src/main/java/com/liwa/liwaso/MainApplication.java
@@ -22,6 +22,7 @@ public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+        System.out.println("---------start success-----------");
     }
 
 }
